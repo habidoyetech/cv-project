221244260cb51df695c021132a62c94942a044fb
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-05-02 18:19:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1 className="header">Abidoye Abiodun Work</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
